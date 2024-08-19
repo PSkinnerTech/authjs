5235856581ffce4dfd53c4992a464b23cb42c7db
PSkinnerTech <patrick@clgcorporation.com>
2024-08-19 09:21:36 -0500

    🧙🏻‍♂️ side quest

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,13 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    theme: {
+        logo: "/logo.png",
+        colorScheme: "auto"
+    },
     adapter: PrismaAdapter(prisma) as any,
-    providers: [Google],
+    providers: [Google, Github],
 })
